760b7f27416a7fcf14c93582ac45f2e395af2916
Dirk <dirkvanrensburg5@gmail.com>
2018-10-30 16:24:42 +0200

    fixed server for login and register

--- server.js
+++ server.js
@@ -10,14 +10,33 @@ const signin = require('./controlers/signin.js');
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    connectionString  : 'postgresql-rigid-28132', //process.env.DATABASE_URL,
-    ssl: true,
+    host : '127.0.0.1',
+    user : 'dirk',
+    password : 'dirk',
+    database : 'series'
   }
+  // connection: {
+  //   connectionString  : 'postgresql-rigid-28132', //process.env.DATABASE_URL,
+  //   ssl: true,
+  // }
 });
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+var whitelist = ['https://api.thetvdb.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Then pass them to cors:
+app.use(cors(corsOptions));
 
 app.get('/',(req, res ) => res.send('it is working'))
 app.post('/signin', (req , res ) => {signin.handleSignin(req , res , knex ,bcrypt)})
